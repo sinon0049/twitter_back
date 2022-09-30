d36ee2d82718b6b2edbe63da947a8f600ca8a754
Me <sinon049@yahoo.com.tw>
2022-09-30 14:59:29 +0800

    Remove annotation

--- models/tweet.js
+++ models/tweet.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   class Tweet extends Model {
     static associate(models) {
       // define association here
-      //Tweet.belongsTo(models.User)
+      Tweet.belongsTo(models.User)
       Tweet.hasMany(models.Reply)
       Tweet.hasMany(models.Like)
     }
