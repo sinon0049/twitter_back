cbb637568456279811ed4873c385fd70adb5d302
Me <sinon049@yahoo.com.tw>
2022-10-11 13:13:15 +0800

    Add GET /followship/following & GET /followship/follower & remove the other routers

--- routes/modules/followship.js
+++ routes/modules/followship.js
@@ -2,42 +2,58 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const db = require('../../models')
-const Followship = db.Followship
 const User = db.User
+const Sequelize = require('sequelize')
+const { notIn } = Sequelize.Op
 
-router.get('/', passport.authenticate('jwt'), async (req, res) => {
-    const userId = req.user.id
-    let followingArr = []
-    await User.findByPk(userId, {
-        include: [{
-           model: User,
-           attributes: ['id', 'name', 'account', 'avatar'],
-           as: 'Followers' 
-        }],
-    })
-    .then(userData => {
-        userData.Followers.forEach(item => {
-            followingArr.push(item.id)
+router.get('/following/:id', passport.authenticate('jwt'), async (req, res) => {
+    try {
+        const currentUser = req.params.id
+        const followingArr = [Number(currentUser)]
+        const followingList = await User.findByPk(currentUser, {
+            attributes: ['id', 'name', 'account', 'avatar', 'cover'],
+            include: [
+                {
+                    attributes: ['id', 'name', 'account', 'avatar', 'cover', 'introduction'],
+                    model: User, as: 'Followings'
+                }
+            ],
         })
-    })
+        followingList.dataValues.Followings.forEach(item => followingArr.push(item.dataValues.id))
+        console.log(followingArr)
+        const unfollowings = await User.findAll({
+            attributes: ['id', 'name', 'account', 'avatar', 'cover', 'introduction'],
+            where: {
+                id: {
+                    [notIn]: followingArr
+                },
+                role: 'user'
+            },
+            raw: true
+        })
+        followingList.dataValues.unfollowings = unfollowings
+        res.json(followingList)
+    } catch (error) {
+        console.log(error)
+    }
+})
 
-    await User.findAll({
-        attributes: ['id', 'name', 'account', 'avatar'],
-        raw: true,
-        where: {
-            role: 'user'
-        }
-    })
-    .then(userData => {
-        return userData.map(user => {
-            return {
-                ...user,
-                isFollower: followingArr.includes(user.id) ? true : false
-            }
+router.get('/follower/:id', passport.authenticate('jwt'), async (req, res) => {
+    try {
+        const followerList = await User.findByPk(req.params.id, {
+            attributes: ['id', 'name', 'account', 'avatar', 'cover', 'introduction'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'name', 'account', 'avatar', 'cover', 'introduction'],
+                    as: 'Followers'
+                }
+            ]
         })
-    })
-    .then(result => { return res.json(result) })
-    //return res.send(followingArr)
+        res.json(followerList)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router
