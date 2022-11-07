8e56adb751d51d1c0fad258d2268b8f7d8fd48f0
Me <sinon049@yahoo.com.tw>
2022-11-07 20:53:04 +0800

    Remove unnecessary code

--- models/index.js
+++ models/index.js
@@ -4,19 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-//////////    sequelize initialization for local environment
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-
-//////////    sequelize initialization for remote environment
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
   dialectOptions: {
@@ -45,6 +34,7 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+//  initialize db
 db.sequelize.sync()
 
 module.exports = db;
