2320698743126035b8dadd0cc2d3145286e4a4e7
Me <sinon049@yahoo.com.tw>
2022-11-07 20:44:35 +0800

    Modify .gitignore and folder name for temporary files

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -7,7 +7,7 @@ const userController = require('../../controllers/userController')
 //multer settings
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'temp/')
+        cb(null, '/tmp/')
     },
     filename: function(req, file, cb) {
         if(file.mimetype === 'image/jpeg') cb(null, file.originalname + '.jpg') 
