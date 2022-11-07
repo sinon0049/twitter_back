b232361edf7367a786f22455b93d4e71aefe5eab
Me <sinon049@yahoo.com.tw>
2022-11-07 20:46:48 +0800

    Add gcp datastore feature for session storage

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8080
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 const session = require('express-session')
+const { DatastoreStore } = require('@google-cloud/connect-datastore')
+const { Datastore } = require('@google-cloud/datastore')
 const cors = require('cors')
 require('dotenv').config()
 
@@ -13,6 +15,13 @@ app.use(session({
     secret: 'secret',
     resave: false,
     saveUninitialized: false,
+    store: new DatastoreStore({
+        kind: 'express-sessions',
+        dataset: new Datastore({
+            projectId: process.env.GCLOUD_PROJECT,
+            keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
+        })
+    })
 }))
 usePassport(app)
 app.use(routes)
