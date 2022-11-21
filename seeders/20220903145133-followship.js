58449e46dbd32b13a1f2002f507f565148012804
Me <sinon049@yahoo.com.tw>
2022-11-21 17:20:05 +0800

    Modify seeder

--- seeders/20220903145133-followship.js
+++ seeders/20220903145133-followship.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('followships', [
+    await queryInterface.bulkInsert('Followships', [
       {
         followerId: 1,
         followingId: 2,
@@ -43,6 +43,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('followships', null, {})
+    await queryInterface.bulkDelete('Followships', null, {})
   }
 };
